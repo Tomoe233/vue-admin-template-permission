5e87426823ddcf40b2f50c0c5604aceae9162ad6
Tomoe <1429521105@qq.com>
2023-01-01 01:33:24 +0800

    升级element-ui为2.15.12；修改菜单管理样式bug

--- src/views/System/menu/index.vue
+++ src/views/System/menu/index.vue
@@ -120,7 +120,7 @@
           </el-col>
           <el-col :span="12">
             <el-form-item label="显示排序" prop="orderNum">
-              <el-input-number v-model="menuForm.orderNum" controls-position="right" :min="1" size="large" />
+              <el-input-number v-model="menuForm.orderNum" controls-position="right" :min="1" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -467,20 +467,6 @@ export default {
   height: auto !important;
 }
 
-::v-deep .el-input__inner {
-  height: 30px;
-  line-height: 30px;
-}
-
-// ::v-deep .el-input-number.is-controls-right .el-input-number__decrease, .el-input-number.is-controls-right .el-input-number__increase {
-//   height: 16px;
-//   line-height: 16px;
-// }
-
-// ::v-deep .el-input-number__decrease, .el-input-number__increase {
-//   top: 4px !important;
-// }
-
 .search {
   display: flex;
   margin-bottom: 10px;
@@ -498,7 +484,7 @@ export default {
   }
 
   .searchButton {
-    margin-left: 10px;
+    margin: auto 10px;
   }
 }
 
